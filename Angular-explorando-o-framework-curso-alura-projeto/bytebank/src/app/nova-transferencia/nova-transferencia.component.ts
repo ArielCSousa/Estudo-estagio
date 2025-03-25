f926e8d8a56904b1f4f41db7f4ef70c8ac78968b
ArielCSousa <arie.sousa@sptech.school>
2025-03-25 15:08:05 -0300

    Curso de Angular - conhecendo a ferramento - finalizado - ALURA

--- Angular-explorando-o-framework-curso-alura-projeto/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ Angular-explorando-o-framework-curso-alura-projeto/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,3 +1,6 @@
+import { Router } from '@angular/router';
+import { Transferencia } from './../models/transferencia.model';
+import { TransferenciaService } from './../services/transferencia.service';
 import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
@@ -12,18 +15,30 @@ export class NovaTransferenciaComponent {
   valor: number;
   destino: number;
 
+  constructor(private service: TransferenciaService, private router: Router){}
+
   transferir() {
     console.log('Solicitada nova transferência');
 
     //chamando o evento criado acima "aoTransferir" e usando "emit()" para EMITIR um valor que é opicional
     //this.aoTransferir.emit({valor: this.valor, destino: this.destino});
     //para melhor visualização da linha acima faremos assim diferente:
-    const valorEmitir = {valor: this.valor, destino: this.destino};
+    const valorEmitir: Transferencia = {
+      valor: this.valor,
+      destino: this.destino
+    };
     //dará no mesmo mas fico melhor de se visualizar
-    this.aoTransferir.emit(valorEmitir);
 
-    this.limparCampo(); //chamando a função para deixar os campos em branco após exibir para o usuário na tela
+    // this.aoTransferir.emit(valorEmitir);
 
+    this.service.adicionar(valorEmitir).subscribe
+      (resultado => {
+      console.log(resultado);
+      this.limparCampo(); //chamando a função para deixar os campos em branco após exibir para o usuário na tela
+      this.router.navigateByUrl('extrato'); //ao clicar no botão TRANSFERIR ele redireciona a página para a página de extrato.
+    },
+      (error) => console.error(error)
+    );
   }
 
   limparCampo(){
