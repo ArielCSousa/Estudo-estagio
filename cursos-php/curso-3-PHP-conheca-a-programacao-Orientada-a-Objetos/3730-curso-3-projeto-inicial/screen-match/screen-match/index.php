1d10d5097027c83995f9656325433e3019886bc2
ArielCSousa <arie.sousa@sptech.school>
2025-06-20 15:30:43 -0300

    progresso do curso 3 de php- orientação a objetos

---
+++ cursos-php/curso-3-PHP-conheca-a-programacao-Orientada-a-Objetos/3730-curso-3-projeto-inicial/screen-match/screen-match/index.php
@@ -0,0 +1,21 @@
+<?php
+
+require __DIR__ . "/src/Modelo/Filme.php";
+
+echo "Bem-vindo(a) ao ScreenMatch\n";
+
+$filme = new Filme();
+// $filme->nome = 'Thor - Ragnarok';
+// $filme->anoLancamento = 2021;
+// $filme->genero = 'super-heroi';
+
+$filme->avalia(10);
+$filme->avalia(6);
+$filme->avalia(7.8);
+$filme->avalia(8.2);
+$filme->avalia(10);
+$filme->avalia(2.2);
+ 
+var_dump($filme);
+
+echo $filme->media() . "\n";
---
+++ cursos-php/curso-3-PHP-conheca-a-programacao-Orientada-a-Objetos/3730-curso-3-projeto-inicial/screen-match/screen-match/src/Modelo/Filme.php
@@ -0,0 +1,21 @@
+<?php
+
+class Filme {
+    private string $nome;
+    private int $anoLancamento;
+    private string $genero;
+    private array $notas = [];
+
+    public function avalia(float $nota): void 
+    {
+        $this->notas[] = $nota;
+    }
+
+    public function media(): float
+    {
+        $somaNotas = array_sum($this->notas);
+        $quantidadeNotas = count($this->notas);
+
+        return $somaNotas / $quantidadeNotas;
+    }
+}
