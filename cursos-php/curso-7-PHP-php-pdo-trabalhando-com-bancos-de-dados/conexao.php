b290cd50878be5520fb70be297ab093ae1a3e35f
ArielCSousa <arie.sousa@sptech.school>
2025-07-16 15:32:22 -0300

    curso de php - PDO

--- cursos-php/curso-7-PHP-php-pdo-trabalhando-com-bancos-de-dados/conexao.php
+++ cursos-php/curso-7-PHP-php-pdo-trabalhando-com-bancos-de-dados/conexao.php
@@ -3,10 +3,29 @@
 // $pdo = new PDO("sqlite:banco.sqlite", $usuario, $senha, [parametros extras]); O PDO ACEITA TODOS ESSES PARAMETROS
 
 $caminhoBanco = __DIR__ . "/banco.sqlite";
-$pdo = new PDO("sqlite:$caminhoBanco");
+$pdo = new PDO('sqlite:'. $caminhoBanco);
 
 echo "Conectei";
 
-$pdo->exec('CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);');
+// $pdo->exec('CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);');
 
-echo "\nTabela criada\n";
+// echo "\nTabela criada\n";
+
+$createTableSql = '
+    CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        birth_date TEXT
+    );
+
+    CREATE TABLE IF NOT EXISTS phones (
+        id INTEGER PRIMARY KEY,
+        area_code TEXT,
+        number TEXT,
+        student_id INTEGER,
+        FOREIGN KEY(student_id) REFERENCES students(id)
+    );
+
+';
+
+$pdo->exec($createTableSql);
--- cursos-php/curso-7-PHP-php-pdo-trabalhando-com-bancos-de-dados/lista-alunos.php
+++ cursos-php/curso-7-PHP-php-pdo-trabalhando-com-bancos-de-dados/lista-alunos.php
@@ -1,11 +1,11 @@
 <?php
 
 use Alura\Pdo\Domain\Model\Student;
+use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
 
 require_once("vendor/autoload.php");
 
-$databasePath = __DIR__ . "/banco.sqlite";
-$pdo = new PDO('sqlite:' . $databasePath);
+$pdo = ConnectionCreator::createConnection();
 
 $statement = $pdo->query('SELECT * FROM students');
 // var_dump($statement->fetchAll());
@@ -21,7 +21,5 @@ foreach ($studentDataList as $studentData) {
 }
 
 var_dump($studentList);
-
-
 
 
